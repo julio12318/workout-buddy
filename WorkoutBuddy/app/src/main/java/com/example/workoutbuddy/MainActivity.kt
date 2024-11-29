091758b0c44ae65930a66eb11ebfa149f68d86b3
unknown <juliocesarmoralesmena1@gmail.com>
2024-11-29 11:45:41 -0500

    fixed some navigation issues

--- WorkoutBuddy/app/src/main/java/com/example/workoutbuddy/MainActivity.kt
+++ WorkoutBuddy/app/src/main/java/com/example/workoutbuddy/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<BottomNavigationView>(R.id.bottom_menu).setOnItemSelectedListener {
+            if (it.itemId==R.id.to_rec_frag) {
+                findNavController(R.id.frag).navigate(R.id.action_global_recomFragment)
+                return@setOnItemSelectedListener true
+            }else if (it.itemId==R.id.to_past_frag) {
+                findNavController(R.id.frag).navigate(R.id.action_global_pastWorkoutFragment)
+                return@setOnItemSelectedListener true
+            } else if (it.itemId==R.id.to_week_frag) {
+                findNavController(R.id.frag).navigate(R.id.action_global_weeklyPlannerFragment)
+                return@setOnItemSelectedListener true
+            } else if (it.itemId==R.id.to_future_frag) {
+                findNavController(R.id.frag).navigate(R.id.action_global_planFragment)
+                return@setOnItemSelectedListener true
+            } else if (it.itemId==R.id.to_profile_frag) {
+                findNavController(R.id.frag).navigate(R.id.action_global_profileFragment)
+                return@setOnItemSelectedListener true
+            }else{
+                return@setOnItemSelectedListener true
+            }
+        }
     }
+
 }
